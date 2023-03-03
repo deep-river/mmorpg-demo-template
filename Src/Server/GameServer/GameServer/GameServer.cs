c54a63ebe000e3f50c3506b447b54285feb009f2
Bangyu Li <by.brad.li@gmail.com>
2023-03-03 17:11:33 +0800

    Add HelloWorld message

--- Src/Server/GameServer/GameServer/GameServer.cs
+++ Src/Server/GameServer/GameServer/GameServer.cs
@@ -24,6 +24,8 @@ namespace GameServer
         {
             network = new NetService();
             network.Init(8000);
+
+            HelloWorldService.Instance.Init();
             //DBService.Instance.Init();
             //var a = DBService.Instance.Entities.Characters.Where(s => s.TID == 1);
             //Console.WriteLine("{0}",a.FirstOrDefault<TCharacter>().Name);
@@ -35,6 +37,9 @@ namespace GameServer
         public void Start()
         {
             network.Start();
+
+            HelloWorldService.Instance.Start();
+
             running = true;
             thread.Start();
         }
