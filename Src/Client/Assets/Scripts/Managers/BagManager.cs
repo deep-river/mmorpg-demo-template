32ec37d642584a8dfbfd64a386c197140ef9f7ac
Bangyu Li <by.brad.li@gmail.com>
2023-04-10 04:03:00 +0800

    fix bag system addItem bug

--- Src/Client/Assets/Scripts/Managers/BagManager.cs
+++ Src/Client/Assets/Scripts/Managers/BagManager.cs
@@ -116,6 +116,7 @@ namespace Managers
                     {
                         this.Items[i].ItemId = (ushort)itemId;
                         this.Items[i].Count = addCount;
+                        break;
                     }
                 }
             }
