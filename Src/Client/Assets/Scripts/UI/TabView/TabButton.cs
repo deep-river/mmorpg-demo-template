5097e19e36030641498f837f1a1610ab5e11832b
Bangyu Li <by.brad.li@gmail.com>
2023-04-11 00:04:02 +0800

    Add resources for quest system

--- Src/Client/Assets/Scripts/UI/TabView/TabButton.cs
+++ Src/Client/Assets/Scripts/UI/TabView/TabButton.cs
@@ -5,31 +5,32 @@ using UnityEngine.UI;
 
 public class TabButton : MonoBehaviour {
 
-	public Sprite activeImage;
-	private Sprite normalImage;
+    public Sprite activeImage;
+    private Sprite normalImage;
 
-	public TabView tabView;
-	public int tabIndex = 0;
-	public bool selected = false;
+    public TabView tabView;
 
-	private Image tabImage;
+    public int tabIndex = 0;
+    public bool selected = false;
+
+    private Image tabImage;
 
 	// Use this for initialization
 	void Start () {
-		tabImage = this.GetComponent<Image>();
-		normalImage = tabImage.sprite;
+        tabImage = this.GetComponent<Image>();
+        normalImage = tabImage.sprite;
 
-		this.GetComponent<Button>().onClick.AddListener(OnClick);
+        this.GetComponent<Button>().onClick.AddListener(OnClick);
 	}
 
-	public void Select(bool select)
-	{
-		tabImage.overrideSprite = select ? activeImage : normalImage;
-	}
+    public void Select(bool select)
+    {
+        tabImage.overrideSprite = select ? activeImage : normalImage;
+    }
 
-	void OnClick()
-	{
-		this.tabView.SelectTab(this.tabIndex);
-	}
-	
+    void OnClick()
+    {
+        this.tabView.SelectTab(this.tabIndex);
+    }
+
 }
