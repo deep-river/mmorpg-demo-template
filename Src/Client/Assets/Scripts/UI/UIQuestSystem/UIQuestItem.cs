c82da67447cd35340d2ab55ad568daa98136b2b1
Bangyu Li <by.brad.li@gmail.com>
2023-04-15 23:39:52 +0800

    Add Quest model, UIQuestSystem, UIQuestItem

---
+++ Src/Client/Assets/Scripts/UI/UIQuestSystem/UIQuestItem.cs
@@ -0,0 +1,39 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIQuestItem : ListView.ListViewItem
+{
+    public Text title;
+
+    public Image background;
+    public Sprite normalBg;
+    public Sprite selectedBg;
+
+    public override void onSelected(bool selected)
+    {
+        this.background.overrideSprite = selected ? selectedBg : normalBg;
+    }
+
+    public Quest quest;
+
+    void Start()
+    {
+
+    }
+
+    bool isEquipped = false;
+
+    public void SetQuestInfo(Quest item)
+    {
+        this.quest = item;
+        if (this.title != null)
+        {
+            this.title.text = this.quest.Define.Name;
+        }
+    }
+}
---
+++ Src/Client/Assets/Scripts/UI/UIQuestSystem/UIQuestSystem.cs
@@ -0,0 +1,86 @@
+﻿using Assets.Scripts.UI.UIQuestSystem;
+using Common.Data;
+using Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIQuestSystem : UIWindow
+{
+    public Text title;
+
+    public GameObject itemPrefab;
+    public TabView Tabs;
+    public ListView listMain;
+    public ListView listBranch;
+
+    public UIQuestInfo questInfo;
+
+    private bool showAvailableList = false;
+
+    void Start()
+    {
+        this.listMain.onItemSelected += this.OnQuestSelected;
+        this.listBranch.onItemSelected += this.OnQuestSelected;
+        this.Tabs.OnTabSelect += OnSelectTab;
+        RefreshUI();
+        //QuestManager.Instance.OnQuestChanged += RefreshUI;
+    }
+
+    void OnSelectTab(int idx)
+    {
+        showAvailableList = idx == 1;
+        RefreshUI();
+    }
+
+    private void OnDestroy()
+    {
+        //QuestManager.Instance.OnQuestChanged -= RefreshUI;
+    }
+
+    void RefreshUI()
+    {
+        ClearAllQuestList();
+        InitAllQuestItems();
+    }
+
+    void InitAllQuestItems()
+    {
+        foreach (var kv in QuestManager.Instance.allQuests)
+        {
+            if (showAvailableList)
+            {
+                if (kv.Value.Info != null)
+                    continue;
+            }
+            else
+            {
+                if (kv.Value.Info == null)
+                    continue;
+            }
+
+            GameObject go = Instantiate(itemPrefab, kv.Value.Define.Type == QuestType.Main);
+            UIQuestItem ui = go.GetComponent<UIQuestItem>();
+            ui.SetQuestInfo(kv.Value);
+            if (kv.Value.Define.Type == QuestType.Main)
+                this.listMain.AddItem(ui as ListView.ListViewItem);
+            else
+                this.listBranch.AddItem(ui as ListView.ListViewItem);
+        }
+    }
+
+    void ClearAllQuestList()
+    {
+        this.listMain.RemoveAll();
+        this.listBranch.RemoveAll();
+    }
+
+    public void OnQuestSelected(ListView.ListViewItem item)
+    {
+        UIQuestItem questItem = item as UIQuestItem;
+        this.questInfo.SetQuestInfo(questItem.quest);
+    }
+}
